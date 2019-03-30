7c822f4220fcf9621bcbe474d6d6fbb3a7112b5e
Kuwata <creep040@gmail.com>
2019-03-30 12:41:43 +0900

    :8

---
+++ ABC/120/D.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <algorithm>
+#include <bitset>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+#include <complex>
+#include <string.h>
+#include <numeric>
+using namespace std;
+
+#define int long long
+#define reps(i,s,n) for(int (i)=(s);(i)<(n);++(i))
+#define rep(i,n) reps(i,0,n)
+#define rept(i,n) rep(i,(n)+1)
+#define repst(i,s,n) reps(i,s,(n)+1)
+#define reprt(i,n,t) for(int (i)=(n);(i)>=(t);--(i))
+#define repr(i,n) reprt(i,n,0)
+#define each(i,v) for(auto &(i):(v))
+#define eachr(i,v) for(auto &(i)=(v).rbegin();(i)!=(v).rend();(i)++)
+#define all(c) (c).begin(),(c).end()
+#define rall(c) (c).rbegin(),(c).rend()
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define tmax(x,y,z) max(x,max(y,z))
+#define tmin(x,y,z) min(x,min(y,z))
+#define chmin(x,y) x=min(x,y)
+#define chmax(x,y) x=max(x,y)
+#define ln '\n'
+#define bln(i,n) (((i)==(n)-1)?'\n':' ')
+#define dbg(x) cout<<#x" = "<<(x)<<ln<<flush
+#define dbga(x,n) {cout<<#x" : ";for(int (i)=0;i<(n);++i){cout<<((x)[i])<<(i==((n)-1)?'\n':' ')<<flush;}}
+#define zero(a) memset(a,0,sizeof(a))
+#define unq(a) sort(all(a)),a.erase(unique(all(a)),a.end())
+#define endl "\n"
+
+//typedef complex<double> P;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<string> vst;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<vector<int> > mat;
+
+const ll inf = (ll)1e9+10;
+const ll linf = (ll)1e18+10;
+const ll mod = (ll)(1e9+7);
+const int dx[] = {0, 1, 0, -1};
+const int dy[] = {1, 0, -1, 0};
+const int ddx[] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int ddy[] = {1, 1, 0, -1, -1, -1, 0, 1};
+const double eps = 1e-10;
+
+ll mop(ll a,ll b,ll m=mod) {ll r=1;a%=m;for(;b;b>>=1){if(b&1)r=r*a%m;a=a*a%m;}return r;}
+ll gcd(ll a,ll b) {return b?gcd(b,a%b):a;}
+ll lcm(ll a,ll b) {return a/gcd(a,b)*b;}
+bool ool(int x,int y,int h,int w) {return((x<0)||(h<=x)||(y<0)||(w<=y));}
+bool deq(double a,double b) {return abs(a-b)<eps;}
+
+struct oreno_initializer {
+	oreno_initializer() {
+		cin.tie(0);
+		ios::sync_with_stdio(0);
+	}
+} oreno_initializer;
+
+
+
+// 各連結成分のサイズが例えば2,3,5のとき答えは8*2 + 5*3 = 31
+// 一番最初の答えは必ずn(n-1)/2
+// サイズa,bの連結成分同士が連結されるとき答えはabだけ減る
+
+struct UF {
+	vector<int> p;
+	UF(int n) : p(n, -1) {};
+	bool unite(int u, int v) {
+		if ((u = find(u)) == (v = find(v))) return false;
+		if (p[u] > p[v]) swap(u, v);
+		p[u] += p[v]; p[v] = u;
+		return true;
+	}
+	bool same(int u, int v) { return find(u) == find(v); }
+	int find(int u) { return p[u] < 0 ? u : p[u] = find(p[u]); }
+	int usize(int u) { return -p[find(u)]; }
+};
+
+int n, m, a[100100], b[100100], s;
+UF uf(100100);
+vector<int> res;
+
+signed main() {
+	cin >> n >> m;
+	rep(i,m) {
+		cin >> a[i] >> b[i];
+		a[i]--, b[i]--;
+	}
+	reverse(a,a+m), reverse(b,b+m);
+	s = n*(n-1)/2;
+	res.push_back(s);
+	rep(i,m-1) {
+		if (!uf.same(a[i],b[i])) s -= uf.usize(a[i]) * uf.usize(b[i]);
+		uf.unite(a[i], b[i]);
+		res.push_back(s);
+	}
+	reverse(all(res));
+	each(i,res) cout << i << endl;
+}
