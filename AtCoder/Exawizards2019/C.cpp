2426b4ab2c2dd7cd4ab183523d66378a0e6f56f1
Kuwata <creep040@gmail.com>
2019-03-31 10:05:11 +0900

    8)

---
+++ AtCoder/Exawizards2019/C.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+#include <algorithm>
+#include <bitset>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+#include <complex>
+#include <string.h>
+#include <numeric>
+using namespace std;
+
+//#define int long long
+#define reps(i,s,n) for(int (i)=(s);(i)<(n);++(i))
+#define rep(i,n) reps(i,0,n)
+#define rept(i,n) rep(i,(n)+1)
+#define repst(i,s,n) reps(i,s,(n)+1)
+#define reprt(i,n,t) for(int (i)=(n);(i)>=(t);--(i))
+#define repr(i,n) reprt(i,n,0)
+#define each(i,v) for(auto &(i):(v))
+#define eachr(i,v) for(auto &(i)=(v).rbegin();(i)!=(v).rend();(i)++)
+#define all(c) (c).begin(),(c).end()
+#define rall(c) (c).rbegin(),(c).rend()
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define tmax(x,y,z) max(x,max(y,z))
+#define tmin(x,y,z) min(x,min(y,z))
+#define chmin(x,y) x=min(x,y)
+#define chmax(x,y) x=max(x,y)
+#define ln '\n'
+#define bln(i,n) (((i)==(n)-1)?'\n':' ')
+#define dbg(x) cout<<#x" = "<<(x)<<ln<<flush
+#define dbga(x,n) {cout<<#x" : ";for(int (i)=0;i<(n);++i){cout<<((x)[i])<<(i==((n)-1)?'\n':' ')<<flush;}}
+#define zero(a) memset(a,0,sizeof(a))
+#define unq(a) sort(all(a)),a.erase(unique(all(a)),a.end())
+#define endl "\n"
+
+//typedef complex<double> P;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<string> vst;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<vector<int> > mat;
+
+const ll inf = (ll)1e9+10;
+const ll linf = (ll)1e18+10;
+const ll mod = (ll)(1e9+7);
+const int dx[] = {0, 1, 0, -1};
+const int dy[] = {1, 0, -1, 0};
+const int ddx[] = {0, 1, 1, 1, 0, -1, -1, -1};
+const int ddy[] = {1, 1, 0, -1, -1, -1, 0, 1};
+const double eps = 1e-10;
+
+ll mop(ll a,ll b,ll m=mod) {ll r=1;a%=m;for(;b;b>>=1){if(b&1)r=r*a%m;a=a*a%m;}return r;}
+ll gcd(ll a,ll b) {return b?gcd(b,a%b):a;}
+ll lcm(ll a,ll b) {return a/gcd(a,b)*b;}
+bool ool(int x,int y,int h,int w) {return((x<0)||(h<=x)||(y<0)||(w<=y));}
+bool deq(double a,double b) {return abs(a-b)<eps;}
+
+struct oreno_initializer {
+	oreno_initializer() {
+		cin.tie(0);
+		ios::sync_with_stdio(0);
+	}
+} oreno_initializer;
+
+
+
+// i番目が左端にはみ出すならその左にいるゴーレムも全部左端にはみ出す 右も同様
+// ニブタン2回
+
+int n, q, l, r, le, ri;
+string s;
+char t[200200], d[200200];
+
+bool outl(int x) {
+	int id = x;
+	rep(i,q) if (s[id]==t[i]) {
+		if (d[i]=='L') id--;
+		else id++;
+		if (id<0) return 1;
+		if (id>=n) return 0;
+	}
+	return 0;
+}
+
+bool outr(int x) {
+	int id = x;
+	rep(i,q) if (s[id]==t[i]) {
+		if (d[i]=='L') id--;
+		else id++;
+		if (id<0) return 0;
+		if (id>=n) return 1;
+	}
+	return 0;
+}
+
+signed main() {
+	cin >> n >> q >> s;
+	rep(i,q) cin >> t[i] >> d[i];
+
+	// left
+	l = -1, r = n;
+	while (r-l>1) {
+		int m = (l+r)/2;
+		(outl(m) ? l : r) = m;
+	}
+	le = l;
+	// right
+	l = -1, r = n;
+	while (r-l>1) {
+		int m = (l+r)/2;
+		(outr(m) ? r : l) = m;
+	}
+	ri = r;
+	cout << n - (le+1 + n-ri) << endl;
+}
